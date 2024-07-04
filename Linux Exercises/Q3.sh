0204414208f0e9941a34eeb8115d142e2d50356d
Vanuja <vanuja.uvaneka@gmail.com>
2024-07-04 15:46:46 +0530

    Update linux answers

--- Linux Exercises/Q3.sh
+++ Linux Exercises/Q3.sh
@@ -3,4 +3,4 @@
 mkdir -p Exercise3
 cd Exercise3
 touch secure.txt
-
+sudo chown root secure.txt
--- Linux Exercises/Q6.sh
+++ Linux Exercises/Q6.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+ping google.com
+nslookup google.com
+netstat -tuln
 
+# -t: Show TCP connections.
+# -u: Show UDP connections.
+# -l: Show listening sockets.
+# -n: Show numerical addresses (do not resolve names).
