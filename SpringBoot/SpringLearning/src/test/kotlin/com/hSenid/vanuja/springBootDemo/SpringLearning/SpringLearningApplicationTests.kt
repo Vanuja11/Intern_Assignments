edc8620af6a317d46a479565b05bc36bcf1e32b6
Vanuja <vanuja.uvaneka@gmail.com>
2024-07-15 14:28:54 +0530

    create SpringBoot project

---
+++ SpringBoot/SpringLearning/src/test/kotlin/com/hSenid/vanuja/springBootDemo/SpringLearning/SpringLearningApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hSenid.vanuja.springBootDemo.SpringLearning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringLearningApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
