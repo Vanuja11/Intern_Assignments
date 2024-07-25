2d7a1cb9d5cc6586931643e4d0ea6e9232c564cc
Vanuja <vanuja.uvaneka@gmail.com>
2024-07-25 17:49:03 +0530

    update ts files

--- React/react-typescript/src/App.tsx
+++ React/react-typescript/src/App.tsx
@@ -47,7 +47,14 @@ function App() {
   };
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-    
+    if (!gameOver) {
+      // Users answer
+      const answer = e.currentTarget.value;
+      console.log(`User's answer: ${answer}`);
+
+      // Check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+    }
   };
 
   const nextQuestion = () => {
@@ -59,22 +66,29 @@ function App() {
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
-      <button className='start' onClick={startTrivia} >
-        start
-      </button>
-      <p className='score' >Score:</p>
-      <p className='' >Loading Questions</p>
-      {/* <QuestionCard 
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className='next' onClick={nextQuestion} >
-        Next Question
-      </button>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className='start' onClick={startTrivia} >
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p className='score' >Score:</p> : null}
+      {loading && <p className='' >Loading Questions...</p>}
+      {!loading && !gameOver && (
+        <QuestionCard 
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+
+      )}
+      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className='next' onClick={nextQuestion} >
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 }
