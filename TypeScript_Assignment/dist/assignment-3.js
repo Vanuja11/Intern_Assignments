d9977cde54610b73633491da8437ad87a42b8d9b
Vanuja <vanuja.uvaneka@gmail.com>
2024-07-31 14:27:41 +0530

    create typescript 3 assignment

--- TypeScript_Assignment/dist/assignment-3.js
+++ TypeScript_Assignment/dist/assignment-3.js
@@ -2,4 +2,19 @@
 function printPet(pet) {
     console.log(`Name: ${pet.name}`);
     console.log(`Age: ${pet.age}`);
+    if ('waterType' in pet) {
+        console.log(`Water Type: ${pet.waterType}`);
+    }
+    console.log();
 }
+const dog = {
+    name: 'Buddy',
+    age: 5
+};
+const shark = {
+    name: 'Fishy',
+    age: 2,
+    waterType: 'saltwater'
+};
+printPet(dog);
+printPet(shark);
--- TypeScript_Assignment/src/assignment-3.ts
+++ TypeScript_Assignment/src/assignment-3.ts
@@ -12,4 +12,23 @@ interface Fish {
 function printPet(pet: Animal | Fish): void {
     console.log(`Name: ${pet.name}`)
     console.log(`Age: ${pet.age}`)
+
+    if ('waterType' in pet) {
+        console.log(`Water Type: ${pet.waterType}`)
+    }
+    console.log()
 }
+
+const dog: Animal = {
+    name: 'Buddy',
+    age: 5
+}
+
+const shark: Fish = {
+    name: 'Fishy',
+    age: 2,
+    waterType: 'saltwater'
+}
+
+printPet(dog)
+printPet(shark)
