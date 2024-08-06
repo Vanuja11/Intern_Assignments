38d08fd241e10e658fd23472fc1f7c93df92809f
Vanuja <vanuja.uvaneka@gmail.com>
2024-08-06 13:31:43 +0530

    create axios react project

---
+++ React/axios/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios'
+
+function App() {
+  const url = 'https://jsonplaceholder.typicode.com/todos'
+  
+  return (
+    <div className="App">
+      HELLOING!!!!!
+    </div>
+  );
+}
+
+export default App;
