0787f59f39fa6d79386a31763e0d13c86aaa2cad
Vanuja <vanuja.uvaneka@gmail.com>
2024-08-06 11:35:09 +0530

    basic api fetching application

--- React/api-application/src/App.tsx
+++ React/api-application/src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const baseURL = "/api/employees";
+
+  const [todos, setTodos] = useState([]);
+  const [error, setError] = useState({});
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      // fetch(baseURL)
+      .then((response) => response.json())
+      .then((res) => setTodos(res.slice(0, 10)))
+      .catch((err) => setError(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">HELLOING!</div>
+      <div>
+        <h1>Employees</h1>
+        {todos.map((todo: any) => (
+          <div key={todo.id}>
+            <p>{todo.title}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
