8b2d4b129178726509553172203f78987c2f595d
Vanuja <vanuja.uvaneka@gmail.com>
2024-08-06 14:23:18 +0530

    update axios project files

--- React/axios/src/App.tsx
+++ React/axios/src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const url = 'https://jsonplaceholder.typicode.com/todos'
-  
+  const [cryptos, setCryptos] = useState([]);
+  useEffect(() => {
+    const url = "https://jsonplaceholder.typicode.com/todos";
+    
+    axios.get(url).then((response) => {
+      setCryptos(response.data);
+    });
+  }, []);
+
+  // empty dependency array []. only executes one time, on the intial page load
+
   return (
     <div className="App">
-      HELLOING!!!!!
+      {cryptos
+        ? cryptos.map((crypto: any) => (
+            <div key={crypto.id}>
+              <p>
+                {crypto.id}. {crypto.title}
+              </p>
+              {/* <p>{crypto}</p> */}
+            </div>
+          ))
+        : null}
     </div>
   );
 }
